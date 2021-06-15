863d989c325550461fd818f24c58076ca74b144d
yisroel7210 <81200488+yisroel7210@users.noreply.github.com>
2021-06-15 17:46:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+alert("it worked");
+alert("ready for the next?");
+document.querySelector('a').style.backgroundColor = "yellow";
+//document.querySelector('button').style.backgroundColor = "black";
+document.querySelectorAll('td')[0].classList.add("huge");
+document.querySelector('h3').innerHTML = "Good and great Working Places";
+//document.querySelector('h5'  'a').innerHTML = "<em>Back</em>";
+document.querySelector('h5').classList.add("huge");
